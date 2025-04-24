e075567821e132381d00744c1db7885ed2a4b531
G-BOUJON <gabriel.o.bjn@gmail.com>
2025-04-24 15:07:14 +0200

    Added IMFilesConnexionInfos

---
+++ ToolBox_MVC/Areas/LicenseManager/Controllers/RemovalController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToolBox_MVC.Areas.LicenseManager.Services;
+using ToolBox_MVC.Services.MFiles.Sync;
+
+namespace ToolBox_MVC.Areas.LicenseManager.Controllers
+{
+    public class RemovalController : Controller
+    {
+        private readonly ILicenseMangagerService _licenseManager;
+        private readonly ISyncService _syncService;
+
+        public RemovalController(ILicenseMangagerService licenseManager, ISyncService syncService)
+        {
+            _licenseManager = licenseManager;
+            _syncService = syncService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> List(int serverId)
+        {
+            var accountsToRemove = await _licenseManager.GetAccountsToRemoveLicenseAsync(serverId);
+            return View(accountsToRemove.OrderBy(a => a.UserName));
+        }
+    }
+}
---
+++ ToolBox_MVC/Services/Repository/MFilesConnexionInfosService.cs
@@ -0,0 +1,33 @@
+﻿using ToolBox_MVC.Models;
+
+namespace ToolBox_MVC.Services.Repository
+{
+    public class MFilesConnexionInfosService : IMFilesConnexionInfosService
+    {
+        private readonly ICredentialRepository _credentialRepository;
+        private readonly IMFilesServerRepository _serverRepository;
+
+        public MFilesConnexionInfosService(ICredentialRepository credentialRepository, IMFilesServerRepository serverRepository)
+        {
+            _credentialRepository = credentialRepository;
+            _serverRepository = serverRepository;
+        }
+
+        public MFilesConnexionInfo GetConnexionInfos(int serverId)
+        {
+            var credentials = _credentialRepository.GetCredentials(serverId);
+            var server = _serverRepository.GetMFilesServer(serverId);
+
+            return new MFilesConnexionInfo
+            {
+                Username = credentials.Username,
+                Password = credentials.Password,
+                Domain = credentials.Domain,
+                ProtocolSequence = server.ProtocolSequence,
+                NetworkAddress = server.NetworkAddress,
+                EndPoint = server.EndPoint,
+                VaultGuid = server.VaultGuid
+            };
+        }
+    }
+}
